3aefa685a10811f65b8d6961cb144e0d734d89fe
Adrian-Developer <soft.developer.adrian@gmail.com>
2020-07-31 17:16:51 +0200

    *Restricts Spawn Rate Range

--- Assets/Scripts/Mechanics/SpawnRateController.cs
+++ Assets/Scripts/Mechanics/SpawnRateController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Spawn Rate Config", menuName = "Spawn Platform/Spawn Rate", order = 1)]
 public class SpawnRateController : ScriptableObject
 {
+    [Range(1f,50f)]
     [SerializeField] float spawnRate; 
 
     public float SpawnRate
