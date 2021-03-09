559ba8e677b71299aaa845f61bbfa8fa5aae8c9b
Adrian-Developer <soft.developer.adrian@gmail.com>
2021-03-09 21:24:56 +0100

    + Adds Unit Test support and initial Player testing

---
+++ Assets/Tests/PlayMode/MainCharacterTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MainCharacterTest
+    {
+        [UnityTest]
+        public IEnumerator MoveLeftTest()
+        {
+            GameObject gameObject = new GameObject();
+            MainCharacter mainCharacter = gameObject.AddComponent<MainCharacter>();
+
+            mainCharacter.Move(Vector3.left);
+
+            Assert.AreEqual(Vector3.left, mainCharacter.transform.position);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator MoveRightTest()
+        {
+            GameObject gameObject = new GameObject();
+            MainCharacter mainCharacter = gameObject.AddComponent<MainCharacter>();
+
+            mainCharacter.Move(Vector3.right);
+
+            Assert.AreEqual(Vector3.right, mainCharacter.transform.position);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator MoveUpTest()
+        {
+            GameObject gameObject = new GameObject();
+            MainCharacter mainCharacter = gameObject.AddComponent<MainCharacter>();
+
+            mainCharacter.Move(Vector3.up);
+
+            Assert.AreEqual(Vector3.up, mainCharacter.transform.position);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator MoveDownTest()
+        {
+            GameObject gameObject = new GameObject();
+            MainCharacter mainCharacter = gameObject.AddComponent<MainCharacter>();
+
+            mainCharacter.Move(Vector3.down);
+
+            Assert.AreEqual(Vector3.down, mainCharacter.transform.position);
+
+            yield return null;
+        }
+    }
+}
