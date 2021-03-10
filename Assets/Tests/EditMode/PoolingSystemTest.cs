1cb23f72975de43de613d477a5ceef62db4f3440
Adrian-Developer <soft.developer.adrian@gmail.com>
2021-03-10 15:26:16 +0100

    +Adds pooling system unit test

--- Assets/Tests/EditMode/PoolingSystemTest.cs
+++ Assets/Tests/EditMode/PoolingSystemTest.cs
@@ -9,9 +9,49 @@ namespace Tests
     public class PoolingSystemTest
     {
         [Test]
-        public void PoolingSystemMethodTest()
+        public void AddItemTest()
         {
-            // Use the Assert class to test conditions
+            GameObject gameObject = new GameObject();
+            MovingPlatform movingPlatform = gameObject.AddComponent<MovingPlatform>();
+
+            List<MovingPlatform> list = new List<MovingPlatform>();
+            list.Add(movingPlatform);
+
+            PoolingSystem<MovingPlatform> pool = new PoolingSystem<MovingPlatform>(list);
+
+            MovingPlatform pooledInstance = pool.GetRandomItem();
+
+            Assert.AreEqual(movingPlatform, pooledInstance);
+        }
+
+        [Test]
+        public void ReleaseItemTest()
+        {
+            GameObject gameObject = new GameObject();
+            MovingPlatform movingPlatform = gameObject.AddComponent<MovingPlatform>();
+
+            List<MovingPlatform> list = new List<MovingPlatform>();
+            list.Add(movingPlatform);
+
+            PoolingSystem<MovingPlatform> pool = new PoolingSystem<MovingPlatform>(list);
+
+            MovingPlatform randomMovingPlatform = pool.GetRandomItem();
+            pool.ReleaseItem(randomMovingPlatform);
+
+            randomMovingPlatform = pool.GetRandomItem();
+
+            Assert.AreEqual(movingPlatform, randomMovingPlatform);
+        }
+
+        [Test]
+        public void EmptyPoolTest()
+        {
+            List<MovingPlatform> list = new List<MovingPlatform>();
+            PoolingSystem<MovingPlatform> pool = new PoolingSystem<MovingPlatform>(list);
+
+            MovingPlatform pooledInstance = pool.GetRandomItem();
+
+            Assert.AreEqual(default(MovingPlatform), pooledInstance);
         }
     }
 }
