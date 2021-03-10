cb4ac50c62b34dca727ef8e6749eba802d4cd903
Adrian-Developer <soft.developer.adrian@gmail.com>
2021-03-10 13:36:08 +0100

    * Updates High Score Data overriding equality operators

--- Assets/Scripts/Persistence/HighScoreData.cs
+++ Assets/Scripts/Persistence/HighScoreData.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 [System.Serializable]
 
 /// <summary>
-/// High Score Data encapsulation to be saved
+/// High Score Data encapsulation
 /// </summary>
 public class HighScoreData
 {
@@ -19,4 +19,36 @@ public class HighScoreData
     {
         HighScore = highScore;
     }
+    
+    public static bool operator ==(HighScoreData hsd1, HighScoreData hsd2)
+    {
+        if ((object)hsd1 == null)
+        {
+            return (object)hsd2 == null;
+        }
+
+        return hsd1.Equals(hsd2);
+    }
+
+    public static bool operator !=(HighScoreData hsd1, HighScoreData hsd2)
+    {
+        return !(hsd1 == hsd2);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        HighScoreData hsd = (HighScoreData)obj;
+        return (HighScore == hsd.HighScore);
+    }
+
+    public override int GetHashCode()
+    {
+        return HighScore.GetHashCode();
+    }
+    
 }
